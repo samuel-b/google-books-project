334d9a2039f12a14eb7f9411971d4f76d7580bae
Sam-B-Hub <64763981+Sam-B-Hub@users.noreply.github.com>
2022-04-07 22:09:54 +1000

    added modal w/ full description and additional info

--- my-app/src/components/BookCard/BookCard.jsx
+++ my-app/src/components/BookCard/BookCard.jsx
@@ -1,10 +1,20 @@
 import styles from "./BookCard.module.scss";
+// import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
 
 const BookCard = ({ book }) => {
-    book.volumeInfo.description = book.volumeInfo.description
+    console.log(book);
+    const shortDesc = book.volumeInfo.description
         .split(" ")
         .splice(0, 20)
         .join(" ");
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleOpen = () => setShow(true);
 
     return (
         <div className={styles.BookCard}>
@@ -19,8 +29,38 @@ const BookCard = ({ book }) => {
             </p>
             <p className={styles.BookCard__Description}>
                 <strong> Description: </strong>
-                {`${book.volumeInfo.description}...`}
+                {`${shortDesc}...`}
             </p>
+            <button className={styles.BookCard__Button} onClick={handleOpen}>
+                More Info
+            </button>
+            <Modal
+                open={show}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description">
+                <Box className={styles.BookCard__Modal}>
+                    <Typography>
+                        <h4>{book.volumeInfo.title}</h4>
+                        <img src={book.volumeInfo.imageLinks.thumbnail} alt="" />
+                        <p>
+                            <strong>Pages: </strong>
+                            {book.volumeInfo.pageCount}
+                        </p>
+                        <p>
+                            <strong>Categories: </strong>
+                            {book.volumeInfo.categories}
+                        </p>
+                        <p>
+                            <strong>Publisher: </strong>
+                            {book.volumeInfo.publisher}
+                        </p>
+
+                        <strong> Full Description </strong>
+                        <p>{book.volumeInfo.description}</p>
+                    </Typography>
+                </Box>
+            </Modal>
         </div>
     );
 };
